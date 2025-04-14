1c1dfeb6dbeadb916d1b1594c1aa643c228d3fcf
Rafsha-Jabin <rafshajabin00@gmail.com>
2025-04-14 11:25:21 +0530

    simple calculator.java

---
+++ simple calculator.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1 = scanner.nextDouble(); 
+        char operator = scanner.next().charAt(0); 
+        double num2 = scanner.nextDouble(); 
+        scanner.close();
+        
+        double result;
+        
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero");
+                    return;
+                }
+                break;
+            case '%':
+                if (num2 != 0) {
+                    result = num1 % num2;
+                } else {
+                    System.out.println("Error: Division by zero");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator");
+                return;
+        }
+        
+        
+        System.out.printf("%.1f\n", result);
+    }
+}
